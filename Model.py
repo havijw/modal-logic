cc4fe845946e38da50d70d805ead4bd63086edc0
havijw <havijw@umich.edu>
2019-11-07 18:04:32 -0500

    Class for a model for modal language

---
+++ Model.py
@@ -0,0 +1,10 @@
+class Model:
+    # requires: worlds is a list of strings
+    #           access is a set of tuples of elements of worlds,
+    #               which define an accessibility function on worlds
+    #           variables is a dictionary consisting of pairs
+    #               proposition:[worlds at which proposition is true]
+    def __init__(self, worlds=[], access=[], variables={}):
+        self.worlds    = worlds
+        self.access    = access
+        self.variables = list(variables.keys())
