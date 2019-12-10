be2556a048570abe77a2dcd8eccd89346b5dc78a
John Haviland <havijw@s-mh-g440-m09.local>
2019-12-09 20:21:15 -0500

    Added functions to test and enforce properties of the model

--- Proof_Model.py
+++ Proof_Model.py
@@ -8,10 +8,52 @@ class Proof_Model(Model):
                 'variables'    : variables, 
                 'propositions' : propositions
             }
+
+    def is_accessible(self, world, world_to_access):
+        if world_to_access in self.worlds[world]['access']:
+            return True
+        return False
     
     def add_access(self, world, world_to_access):
-        if world_to_access not in self.worlds[world]['access']:
+        if not self.is_accessible(world, world_to_access):
             self.worlds[world]['access'].append(world_to_access)
+    
+    def is_transitive(self):
+        for world in self.worlds:
+            for a_world in self.worlds[world]['access']:
+                for a_a_world in self.worlds[a_world]['access']:
+                    if not self.is_accessible(world, a_a_world):
+                        return False
+        return True
+    
+    def make_transitive(self):
+        while not self.is_transitive():
+            for world_1 in self.worlds:
+                for world_2 in self.worlds[world_1]['access']:
+                    for world_3 in self.worlds[world_2]['access']:
+                        self.add_access(world_1, world_3)
+    
+    def is_symmetric(self):
+        for world in self.worlds:
+            for a_world in self.worlds[world]['access']:
+                if not self.is_accessible(a_world, world):
+                    return False
+        return True
+    
+    def make_symmetric(self):
+        for world in self.worlds:
+            for a_world in self.worlds[world]['access']:
+                self.add_access(a_world, world)
+    
+    def is_reflexive(self):
+        for world in self.worlds:
+            if not self.is_accessible(world, world):
+                return False
+        return True
+    
+    def make_reflexive(self):
+        for world in self.worlds:
+            self.add_access(world, world)
     
     def __str__(self):
         string_rep = ''
@@ -33,4 +75,4 @@ class Proof_Model(Model):
                 string_rep += '\n    * ' + proposition + ' -- ' + str(self.worlds[world]['propositions'][proposition])
             string_rep += '\n'
         
-        return string_rep
+        return string_rep + '\n--------------------------------'
